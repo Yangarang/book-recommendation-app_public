0e0e24fa3bb583a5bebcd0751929ecd7f7391d6d
Yangarang <jonyang7777@gmail.com>
2020-08-05 02:24:15 -0400

    NLP
    Added NLP Recommendation Page

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('book/<int:pk>/', views.book, name='book'),
+    path('nlpbook/<int:pk>/', views.nlpbook, name='nlpbook'),
     path('search/', views.search, name='search'),
+    path('nlpsearch/', views.nlpsearch, name='nlpsearch'),
     path('about/', views.about, name='about')
 ]
--- home/views.py
+++ home/views.py
@@ -4,7 +4,10 @@ from django.views.generic import ListView
 from multiprocessing.pool import ThreadPool
 from django.core.paginator import Paginator
 from .models import get_books_homepage, get_book_details
-from .models import	get_search_results, get_same_author_books, get_same_genre_books
+from .models import	get_search_results, get_same_author_books, get_same_genre_books, get_nlpsearch_results
+from .nlp_bookdata import get_nlpbook_recommendations
+
+import time
 
 # Create your views here.
 def home(request):
@@ -22,16 +25,11 @@ def home(request):
 
 
 def book(request, pk):	
-	#### OLD UNUSED MULTI-THREADING ####
-	# t1 = threading.Thread(target=get_book_details(),args=[pk,], name='t1')
-	# t2 = threading.Thread(target=get_recommended_books(),args=[pk,], name='t2')
-	# t1.start()
-	# t2.start()
-	####################
+	start = time.time()
 	pool = ThreadPool(processes=3)
 	async_result1 = pool.apply_async(get_book_details,kwds={'pk': pk})
 	async_result2 = pool.apply_async(get_same_author_books,kwds={'pk': pk}) 
-	async_result3 = pool.apply_async(get_same_genre_books,kwds={'pk': pk}) 
+	async_result3 = pool.apply_async(get_same_genre_books,kwds={'pk': pk})
 	context = {
 		# Multi-threaded calls
 		'books': async_result1.get(),
@@ -39,9 +37,27 @@ def book(request, pk):
 		'genres': async_result3.get()
 		# Non-threaded calls
 		# 'books': get_book_details(pk=pk),
-		# 'recommendations': get_recommended_books(pk=pk),
+		# 'recommendations': get_same_author_books(pk=pk),
+		# 'genres': get_same_genre_books(pk=pk)
 	}
+	end = time.time()
+	print("The time it took to run these threads was {}".format(end-start))
 	return render(request, 'home/book.html', context)
+
+
+def nlpbook(request, pk):	
+	start = time.time()
+	pool = ThreadPool(processes=3)
+	async_result1 = pool.apply_async(get_book_details,kwds={'pk': pk})
+	async_result2 = pool.apply_async(get_nlpbook_recommendations,kwds={'pk': pk}) 
+
+	context = {
+		'books': async_result1.get(),
+		'recommendations': async_result2.get()
+	}
+	end = time.time()
+	print("The time it took to run these threads was {}".format(end-start))
+	return render(request, 'home/nlpbook.html', context)
 
 # def search(request):
 # 	query = request.GET.get('results')
@@ -66,6 +82,21 @@ def search(request):
 		}
 		return render(request, 'home/search.html', context)
 	return render(request, 'home/search.html', {'init': 'init', 'title': 'Search'},)
+
+def nlpsearch(request):
+	query = request.GET.get('results')
+	if query:
+		page = request.GET.get('page', 1)
+		books = get_nlpsearch_results(results=query)
+		# paginate all book returns
+		paginator = Paginator(books, 10)
+		books = paginator.get_page(page)
+		context = {
+			'books': books,
+			'query': query,
+		}
+		return render(request, 'home/nlpsearch.html', context)
+	return render(request, 'home/nlpsearch.html', {'init': 'init', 'title': 'Search'},)
 
 def about(request):
 	return render(request, 'home/about.html', {'title': 'About'})
